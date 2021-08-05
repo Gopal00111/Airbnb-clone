13bcd2cb9c4af0fac79a2c6048bfe70495158d32
Gopal Mer <mergopal553@gmail.com>
2021-08-05 18:26:46 +0530

    airbnb-homepage

---
+++ components/Footer.js
@@ -0,0 +1,39 @@
+export default function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">ABOUT</h5>
+            <p>How Airnb works</p>
+            <p>Newsroom</p>
+            <p>Investor</p>
+            <p>Airnb plus</p>
+            <p>Airnb Luxe</p>
+            </div>
+          
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">COMMUNITY</h5>
+            <p>How Airnb works</p>
+            <p>Newsroom</p>
+            <p>Investor</p>
+            <p>Airnb plus</p>
+            <p>Airnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">HOST</h5>
+            <p>How Airnb works</p>
+            <p>Newsroom</p>
+            <p>Investor</p>
+            <p>Airnb plus</p>
+            <p>Airnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+            <h5 className="font-bold">SUPPORT</h5>
+            <p>How Airnb works</p>
+            <p>Newsroom</p>
+            <p>Investor</p>
+            <p>Airnb plus</p>
+            <p>Airnb Luxe</p>
+            </div>
+        </div>
+    )
+}
